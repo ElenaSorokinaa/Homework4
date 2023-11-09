287da507e022f62f048c6bf28f6f7e5e8de0ad9c
ElenaSorokinaa <149004479+ElenaSorokinaa@users.noreply.github.com>
2023-11-09 17:25:54 +0300

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,13 @@
+package TernaryOperator;
+
+import java.util.Scanner;
+
+public class Ternary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = scanner.nextInt();
+        String result= number > 0 ? number % 7 < 4 ? "positive-m" : "positive-p" : number == 0 ? "zero" : number % 7 > -4 ? "negative-m" : "negative-p";
+        System.out.println(result);
+    }
+}
